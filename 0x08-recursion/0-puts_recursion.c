bc0e81fcdbc9ccf27ffca465fbf29305b3457e3b
Daniel Watchman <dannietortee@gmail.com>
2023-10-26 22:42:35 -0700

    fixes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: pointer with the address of the string
- * Return: Always 0.
- */
+* _puts_recursion - prints a string, followed by a new line
+* @s: pointer to string
+* Return: nothing
+**/
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-		_puts_recursion(&s[i]);
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,22 +1,14 @@
 #include "main.h"
 /**
- * _print_rev_recursion- print a string in reverse
- * @s: pointer that have the address of the string
- * Return always 0
- */
+* _print_rev_recursion -  prints a string, in reverse.
+* @s: pointer to string
+* Return: nothing
+**/
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
+	if (*s != '\0')
 	{
-		i++;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else
-	{
-		return;
-	}
-	_print_rev_recursion(&s[i]);
-	i--;
-	_putchar(s[i]);
 }
